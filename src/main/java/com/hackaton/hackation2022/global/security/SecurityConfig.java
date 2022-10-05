d5b5dd578b578c565fda954535798705d0c030bd
gimhanul <gimhanuly@gmail.com>
2022-10-05 20:02:07 +0900

    ♻️ :: (#11) security config

--- src/main/java/com/hackaton/hackation2022/global/security/SecurityConfig.java
+++ src/main/java/com/hackaton/hackation2022/global/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsUtils;
 
 @Configuration
 @EnableWebSecurity
@@ -21,11 +22,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
-                .httpBasic().disable()
                 .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .formLogin().disable()
+                .cors()
+
                 .and()
-                .formLogin().disable();
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
 
         return http.build();
     }
