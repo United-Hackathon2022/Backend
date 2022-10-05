fed06a7711e2a74ab768f90a7a592a50001a0285
gimhanul <gimhanuly@gmail.com>
2022-10-06 02:19:51 +0900

    ♻️ :: FeedRequest NotNull -> NotBlank

--- src/main/java/com/hackaton/hackation2022/domain/feed/presentation/dto/request/FeedRequest.java
+++ src/main/java/com/hackaton/hackation2022/domain/feed/presentation/dto/request/FeedRequest.java
@@ -6,22 +6,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 public class FeedRequest {
 
-    @NotNull
+    @NotBlank
     @Length(max = 30)
     private String title;
 
-    @NotNull
+    @NotBlank
     @Length(max = 1000)
     private String content;
 
-    @NotNull
+    @NotBlank
     @Length(max = 8)
     private FeedType type;
 }
