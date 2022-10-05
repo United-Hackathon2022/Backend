81307b582b1790a698d1f191f9805b9323815f1f
gimhanul <gimhanuly@gmail.com>
2022-10-05 19:27:13 +0900

    📄 :: (#6) Comment Entity
    - comment entity
    - comment repository

---
+++ src/main/java/com/hackaton/hackation2022/domain/feed/domain/Comment.java
@@ -0,0 +1,38 @@
+package com.hackaton.hackation2022.domain.feed.domain;
+
+import com.hackaton.hackation2022.domain.user.domain.entity.User;
+import com.hackaton.hackation2022.global.entity.BaseTimeIdEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "tbl_comment")
+public class Comment extends BaseTimeIdEntity {
+
+    @NotNull
+    @Length(max = 1000)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_id", nullable = false)
+    private Feed feed;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Builder
+    public Comment(String content, Feed feed, User user) {
+        this.content = content;
+        this.feed = feed;
+        this.user = user;
+    }
+}
