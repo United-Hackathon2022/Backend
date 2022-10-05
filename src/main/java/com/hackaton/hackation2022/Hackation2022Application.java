1833c8779eb9625be38da1250d83d87611b9d592
lyutvs <lyutvs@gmail.com>
2022-10-05 15:53:46 +0900

    ⚙️ :: 프로젝트 생성

---
+++ src/main/java/com/hackaton/hackation2022/Hackation2022Application.java
@@ -0,0 +1,14 @@
+package com.hackaton.hackation2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackation2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hackation2022Application.class, args);
+    }
+
+}
+
