5743b0d18ba4113198d3a5884b2b10283b708115
lyutvs <lyutvs@gmail.com>
2022-10-05 19:54:04 +0900

    📑 :: MentorApplication

---
+++ src/main/java/com/hackaton/hackation2022/domain/user/domain/MentorApplication.java
@@ -0,0 +1,31 @@
+package com.hackaton.hackation2022.domain.user.domain;
+
+import com.hackaton.hackation2022.global.entity.BaseTimeIdEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "tbl_mentor_application")
+public class MentorApplication extends BaseTimeIdEntity {
+
+    @EmbeddedId
+    private MentorApplicationId id;
+
+    @MapsId("user")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @MapsId("user")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "target_user_id", nullable = false)
+    private User targetUser;
+
+}
