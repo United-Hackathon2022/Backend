4d6bffdec87a21aeb57fa4570d4215f413466613
lyutvs <lyutvs@gmail.com>
2022-10-06 19:29:37 +0900

    ♻️ :: 추가

--- src/main/java/com/hackaton/hackation2022/global/security/SecurityConfig.java
+++ src/main/java/com/hackaton/hackation2022/global/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.hackaton.hackation2022.global.security.jwt.filter.JwtFilterConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -43,6 +44,7 @@ public class SecurityConfig {
                 .and()
                 .authorizeRequests()
                 .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                .antMatchers(HttpMethod.GET, "/auth/naver/redirect").permitAll()
 
                 .and()
                 .apply(new JwtFilterConfig(jwtTokenProvider, objectMapper));
