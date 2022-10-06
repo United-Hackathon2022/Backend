385d1cf083458a5b12d17ab4b5c53a1488223bfc
lyutvs <lyutvs@gmail.com>
2022-10-07 03:07:20 +0900

    ⚡️ :: 추기ㅏ

--- src/main/java/com/hackaton/hackation2022/domain/feed/presentation/CommentController.java
+++ src/main/java/com/hackaton/hackation2022/domain/feed/presentation/CommentController.java
@@ -1,12 +1,23 @@
 package com.hackaton.hackation2022.domain.feed.presentation;
 
 
+import com.hackaton.hackation2022.domain.feed.presentation.dto.request.CreateCommentRequest;
+import com.hackaton.hackation2022.domain.feed.service.CreateCommentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/comments")
 @RequiredArgsConstructor
 public class CommentController {
+    private final CreateCommentService createCommentService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/{feed-id}")
+    public void createComment(@PathVariable("feed-id") Long feedId, @RequestBody @Valid CreateCommentRequest request) {
+        createCommentService.execute(request, feedId);
+    }
 }
