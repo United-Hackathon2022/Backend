49675ddcb5b317d312f594f546b5655cecc20703
gimhanul <gimhanuly@gmail.com>
2022-10-06 13:46:48 +0900

    ⚡️ ::  delete child first

--- src/main/java/com/hackaton/hackation2022/domain/feed/domain/repository/CommentRepository.java
+++ src/main/java/com/hackaton/hackation2022/domain/feed/domain/repository/CommentRepository.java
@@ -1,7 +1,10 @@
 package com.hackaton.hackation2022.domain.feed.domain.repository;
 
 import com.hackaton.hackation2022.domain.feed.domain.Comment;
+import com.hackaton.hackation2022.domain.feed.domain.Feed;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CommentRepository extends CrudRepository<Comment, Long>, CommentRepositoryCustom {
+
+    void deleteCommentsByFeed(Feed feed);
 }
--- src/main/java/com/hackaton/hackation2022/domain/feed/service/DeleteFeedService.java
+++ src/main/java/com/hackaton/hackation2022/domain/feed/service/DeleteFeedService.java
@@ -1,11 +1,16 @@
 package com.hackaton.hackation2022.domain.feed.service;
 
+import com.hackaton.hackation2022.domain.feed.domain.Comment;
 import com.hackaton.hackation2022.domain.feed.domain.Feed;
+import com.hackaton.hackation2022.domain.feed.domain.repository.CommentRepository;
+import com.hackaton.hackation2022.domain.feed.domain.repository.CommentRepositoryCustom;
 import com.hackaton.hackation2022.domain.feed.domain.repository.FeedRepository;
 import com.hackaton.hackation2022.domain.feed.facade.FeedFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,12 +18,15 @@ public class DeleteFeedService {
 
     private final FeedRepository feedRepository;
     private final FeedFacade feedFacade;
+    private final CommentRepository commentRepository;
 
     @Transactional
     public void execute(Long feedId) {
         Feed feed = feedFacade.getFeedById(feedId);
         // TODO :: getCurrentUser
         feed.validateUser(null);
+
+        commentRepository.deleteCommentsByFeed(feed);
         feedRepository.delete(feed);
     }
 }
