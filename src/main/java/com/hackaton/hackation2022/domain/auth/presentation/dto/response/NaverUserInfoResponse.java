2bbd38f97447d4ad1d9a76a2724c71ae1e38f41a
lyutvs <lyutvs@gmail.com>
2022-10-06 14:31:08 +0900

    📑 :: NaverUserInfoResponse

---
+++ src/main/java/com/hackaton/hackation2022/domain/auth/presentation/dto/response/NaverUserInfoResponse.java
@@ -0,0 +1,10 @@
+package com.hackaton.hackation2022.domain.auth.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class NaverUserInfoResponse {
+    private final NaverUserINfoElement response;
+}
